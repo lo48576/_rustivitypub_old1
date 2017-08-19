e469b344f761d1271e3a3e25beb8ad71725d835c
YOSHIOKA Takuma <tashioka.256@gmail.com>
2017-08-19 23:04:01 +0900

    Initial commit

---
+++ rustivitypub/src/lib.rs
@@ -0,0 +1,8 @@
+//! ActivityPub implementation for Rust.
+#![warn(missing_docs)]
+// Allow unknown lints for clippy.
+#![allow(unknown_lints)]
+// `doc_markdown` by clippy says:
+//
+// > warning: you should put `ActivityPub` between ticks in the documentation
+#![allow(doc_markdown)]
