2d1737c96423521e8c4562310f7f783080ef274e
YOSHIOKA Takuma <tashioka.256@gmail.com>
2017-09-04 23:07:30 +0900

    Add `TryFromJsonValue` trait to create views for objects and properties

--- rustivitypub/src/document/view/mod.rs
+++ rustivitypub/src/document/view/mod.rs
@@ -1,5 +1,27 @@
 //! Object views.
 
+use document::JsonValue;
 pub use self::error::{Result, PropertyError};
 
 pub mod error;
+
+
+/// Attempt to construct `Self` via a conversion.
+///
+/// NOTE: Use
+/// [`std::convert::TryFrom`](https://doc.rust-lang.org/nightly/std/convert/trait.TryFrom.html)
+/// when it is stabilized.
+pub trait TryFromJsonValue<'a>: Sized {
+    /// Performs the conversion.
+    fn try_from_json_value(value: &'a JsonValue) -> Result<Self>;
+
+    /// Checks if the `Self` type object can be created from the given JSON value.
+    ///
+    /// Returns `Err(_)` if the given JSON value cannot be converted into `Self`.
+    /// Note that returned `Ok(())` doesn't always means the type conversion would succeed.
+    ///
+    /// This should be called at the head of `try_from_json_value()`.
+    fn validate_json_value(_value: &JsonValue) -> Result<()> {
+        Ok(())
+    }
+}
